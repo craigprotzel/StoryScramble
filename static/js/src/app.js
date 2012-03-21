6d5ba10191e22bd32e69633ae31b1ffd035c1790
craigprotzel <craigprotzel@gmail.com>
2012-03-20 21:51:34 -0400

    level II is working with first db entry

--- static/js/src/app.js
+++ static/js/src/app.js
@@ -1,7 +1,30 @@
-
-//some functionality for usa today button
-$("#usaTodayCall").click(function() {
-  //alert("USA Today API has been clicked."); 
-  //return false;
-
+//code to reveal clue
+$(".clueButtons").click(function() {
+  $('.hidden').hide();
+  $('.clue').show();
 });
+
+
+//code to compare text entry to the correct answer	
+var levelTwoEnter = document.getElementById('levelTwoEnter'); 
+
+var levelTwoEntry = document.getElementById('levelTwoEntry');
+var levelTwoAnswer = document.getElementById('levelTwoAnswer');
+							
+levelTwoEnter.addEventListener('click', checkLevelTwoAnswer, false); 
+				
+function checkLevelTwoAnswer() {
+	console.log("Level 2 Answer Submitted");
+	console.log("You Entered: " + levelTwoEntry.value);
+	console.log("The Correct Answer is: " + levelTwoAnswer.innerHTML);
+	
+	if (levelTwoEntry.value == levelTwoAnswer.innerHTML) {
+		alert("CORRECT");
+		}
+	else {
+		alert("Sorry.Try Again.");
+		}	
+};
+
+			
+			
